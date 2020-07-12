503bb589d9ef7e9d30e2a221385bb86679f95878
msyphus <mark.syphus@gmail.com>
2020-07-11 19:31:10 -0600

    created MVC directories and files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,6 +11,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+app.use(routes);
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks'
+);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
